ac72faa198f613d5ea7ee004e7b1d4f1b7ca1408
Ryan M <rmccauleymusic@gmail.com>
2019-10-23 02:39:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
 if array.length <= 2
   return array.join(" and ")
 else array.length >= 3
-  array[last]  <<(" and ")
+  array.last  <<(" and ")
   array.join(", ")
   return array
 end

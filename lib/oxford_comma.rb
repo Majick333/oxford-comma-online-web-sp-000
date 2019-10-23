af4d0e22b3872558403d12c607eaa9ca2a46b0da
Ryan M <rmccauleymusic@gmail.com>
2019-10-23 02:02:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ if array.length <= 2
   return array.join(" and ")
 else array.length > 2
   big_array = array.join(", ")
-    big_array.insert(last, " and ") 
+    big_array.insert(big_array.last, " and ") 
 end
 end
